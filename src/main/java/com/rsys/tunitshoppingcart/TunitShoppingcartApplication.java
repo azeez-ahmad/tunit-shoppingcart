b0194ad120d0a92d1f0e69442958dfa86bf1477e
azeez-ahmad <azeez.ahmad@jdpa.com>
2023-01-19 13:48:29 -0500

    Initial commit

---
+++ src/main/java/com/rsys/tunitshoppingcart/TunitShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.rsys.tunitshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunitShoppingcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunitShoppingcartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsys/tunitshoppingcart/TunitShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsys.tunitshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunitShoppingcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
